4e45a976942b7621d155c0a8efda4d55a2e22951
Jiny <moc24@naver.com>
2024-05-17 22:36:34 +0900

    fix : 메타데이터 수정- data-rh 추가

--- src/components/SetMetaTag/SetMetaTag.js
+++ src/components/SetMetaTag/SetMetaTag.js
@@ -6,17 +6,21 @@ const SetMetaTag = props =>{
     // props로 content 내용을 불러온다.
     return(
         <Helmet>
-            {/* <title>안녕</title> */}
+            <title>안녕</title>
             {/* <!-- 제목 --> : "울청스타 IT직업 & 유명인 MBTI 테스트" */}
-            <meta property="og:title" content={`SetMeta_title : 나와 잘 맞는 IT직업 & 유명인은 : ${props.title} title`}/>
+            <meta
+                property="og:title" content={`SetMeta_title : 나와 잘 맞는 IT직업 & 유명인은 : ${props.title} title`}
+                
+            />
             {/* <!-- 설명 -->  : "울청스타 IT직업 & 유명인 MBTI 테스트 해보기! 나의 유형은? */}
             <meta
                 property="og:description"
                 content={`SetMeta_des : 나와 잘 맞는 IT직업 & 유명인은 : ${props.description} `}
-                data-react-helmet="true"
             />
             {/* <!-- 이미지 --> */}
-            <meta property="og:image" content={props.image} data-react-helmet="true"/>
+            <meta 
+                property="og:image" content={props.image} 
+                />
             {/* <!-- 링크할 주소 --> */}
             <meta property="og:url" content="https://Ji-ny.github.io/mbti_test_ulsan"/>
         </Helmet>
