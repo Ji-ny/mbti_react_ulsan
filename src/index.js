56e20b3517d9c07b477f1bab269cf1684173292a
Jiny <moc24@naver.com>
2024-05-17 23:07:22 +0900

    fix : react-snap 추가

--- src/index.js
+++ src/index.js
@@ -6,16 +6,35 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
+const container = document.getElementById('root');
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <HelmetProvider>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </HelmetProvider>
-);
+
+
+if (container.hasChildNodes()) {
+  ReactDOM.hydrateRoot(
+    container,
+    <HelmetProvider>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </HelmetProvider>
+  );}
+else{
+  root.render(
+    <HelmetProvider>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </HelmetProvider>
+  );
+}
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
